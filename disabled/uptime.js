e26f236ed7d227d1d978c0724b4e20eb9a201b03
Wahlb3rg <lauritz112@live.dk>
2022-07-30 12:41:05 +0200

    Forsøgte og fejlede

--- disabled/uptime.js
+++ disabled/uptime.js
@@ -9,6 +9,16 @@ module.exports = {
 
     async execute(interaction, client) {
 
+        let totalSeconds = (client.uptime / 1000);
+        let days = Math.floor(totalSeconds / 86400);
+        totalSeconds %= 86400;
+        let hours = Math.floor(totalSeconds / 3600);
+        totalSeconds %= 3600;
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = Math.floor(totalSeconds % 60);
+        
+        let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+
         /*let totalSeconds = (client.uptime / 1000);
         let hours = Math.floor(totalSeconds / 3600);
         totalSeconds %= 3600;
@@ -23,7 +33,7 @@ module.exports = {
 
 
         await interaction.reply({
-            content: 'Ja det virker ikke helt endnu',
+            content: uptime,
             //embeds: [uptimeEmbed],
             ephemeral: true
         });
