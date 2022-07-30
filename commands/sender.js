3cfbd4fa92d059621e53c35ba139a8b8ff5a483a
Wahlb3rg <lauritz112@live.dk>
2022-07-30 12:12:57 +0200

    Så virker send

--- commands/sender.js
+++ commands/sender.js
@@ -6,8 +6,8 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('send')
         .setDescription('Sender en besked som botten')
-        .addStringOption(option => option.setName('title').setDescription('En title til beskeden'))
-        .addStringOption(option => option.setName('tekst').setDescription('Hvad skal botten sige?').setRequired(true)),
+        .addStringOption(option => option.setName('tekst').setDescription('Hvad skal botten sige?').setRequired(true))
+        .addStringOption(option => option.setName('title').setDescription('En title til beskeden').setRequired(false)),
     async execute(interaction) {
 
         var Embed = new MessageEmbed()
