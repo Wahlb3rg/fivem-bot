4eaccafdc878f8a25d9a6f84e2057539c9758a27
Wahlb3rg <lauritz112@live.dk>
2022-08-09 14:41:32 +0200

    Nu kan man vælge om man skal ping eller ej og om der skal en titel på eller ej

--- commands/sender.js
+++ commands/sender.js
@@ -7,14 +7,28 @@ module.exports = {
         .setName('send')
         .setDescription('Sender en besked som botten')
         .addStringOption(option => option.setName('tekst').setDescription('Hvad skal botten sige?').setRequired(true))
+        .addBooleanOption(option => option.setName('ping').setDescription('Skal ping rollen pinges').setRequired(false))
         .addStringOption(option => option.setName('title').setDescription('En title til beskeden').setRequired(false)),
     async execute(interaction) {
 
-        var Embed = new MessageEmbed()
-            .setTitle(interaction.options.getString('title'))
-            .setDescription(interaction.options.getString('tekst'))
-            .setColor(0xdd6544)
+        let Embed;
+        if (interaction.options.getString('title') === null) {
+            Embed = new MessageEmbed()
+                .setDescription(interaction.options.getString('tekst'))
+                .setColor(0xdd6544)
+                .setFooter({text: botconfig.footerText});
+        } else {
+            Embed = new MessageEmbed()
+                .setDescription(interaction.options.getString('tekst'))
+                .setColor(0xdd6544)
+                .setFooter({ text: botconfig.footerText })
+                .setTitle(interaction.options.getString('title'));
+        }
 
-        await interaction.channel.send({ embeds: [Embed] });
+        if (interaction.options.getBoolean('ping') === true) {
+            await interaction.channel.send({ embeds: [Embed], content: '<@&842817228248449074>' });
+        } else {
+            await interaction.channel.send({ embeds: [Embed] });
+        }
     },
 };
